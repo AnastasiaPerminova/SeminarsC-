d7eeaeb953221299f575906338a923da4bbae844
Anastasia Perminova <tasitenok1987@yandex.ru>
2022-11-17 01:24:22 +0300

    HomeWork2Task13 is done

--- HomeWork2Task13/Program.cs
+++ HomeWork2Task13/Program.cs
@@ -6,3 +6,25 @@
 
 // 32679 -> 6
 
+Console.WriteLine("Введите  число: ");
+int number =Math.Abs( Convert.ToInt32(Console.ReadLine ()));
+
+if (number < 100)
+{
+    Console.WriteLine($"В числе третьей цифры нет");
+}
+else if (number > 99 && number <1000)
+{
+    Console.WriteLine($"В числе третья цифра {number%10}");
+
+}
+else
+{   do
+    
+    {
+        number = number / 10;
+    }
+    while (number > 999);
+       
+    Console.WriteLine($"В числе третья цифра {number%10}");
+}
