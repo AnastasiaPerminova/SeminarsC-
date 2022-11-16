ce1c8f2ffe69468edfc1e086fc8ddaca24bf9786
Anastasia Perminova <tasitenok1987@yandex.ru>
2022-11-17 00:04:19 +0300

    added some changes to task11

--- task11/Program.cs
+++ task11/Program.cs
@@ -10,4 +10,4 @@ Console.WriteLine($"Случайное число {number}");
 int digit1 = number/100;
 int digit3 = number % 10;
 
-Console.WriteLine($"-> {digit1}{digit3}");
+Console.WriteLine($"Получается число {digit1*10 + digit3}");
